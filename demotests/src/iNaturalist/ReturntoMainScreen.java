e969b794648d270efebf04091518761176483d43
Owner <Owner@DESKTOP-QCSS2MM.cable.rcn.com>
2024-08-21 12:52:38 -0400

    Created ReturntoMainScreen class, added steps to SearchForTaxon

--- demotests/src/iNaturalist/ReturntoMainScreen.java
+++ demotests/src/iNaturalist/ReturntoMainScreen.java
@@ -1,19 +1,18 @@
-package iNaturalist.Lists;
+package iNaturalist;
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.chrome.ChromeDriver;
 import java.io.File;
 import java.io.FileInputStream;
-import java.io.FileNotFoundException;
 import java.io.IOException;
 import java.util.Properties;
 import java.util.concurrent.TimeUnit;
 
-public class SearchForTaxon {
+
+public class ReturntoMainScreen {
 
 	public static void main(String[] args) throws IOException {
-		// TODO Auto-generated method stub
 		// Specify the file location I used . operation here because
 		//we have object repository inside project directory only
 		File src=new File("jirarepo.properties");
@@ -31,21 +30,18 @@ public class SearchForTaxon {
 		System.setProperty("webdriver.chrome.driver","C:\\Users\\Owner\\OneDrive - Computer Aid, Inc\\Documents\\ChromeDriver\\chromedriver.exe");
 		WebDriver driver = new ChromeDriver();
 		
-		String taxon = "";
-		driver.findElement(By.cssSelector("#Lifelist > div.FlexGrid > div.FlexCol.tree-col > div.form-group.TaxonAutocomplete > div > span")).sendKeys(taxon);
-		driver.findElement(By.cssSelector("#ui-id-69 > div > div.ac-label > div > span.title")).click();
-		
-		String commonname = driver.findElement(By.className("comname display-names")).toString();
-		String scientificname = driver.findElement(By.className("sciname.species.secondary-name")).toString();
-		
-		if (taxon.contentEquals(commonname)){
-			System.out.println("Common name matches!");
-		}else if (taxon.contentEquals(scientificname)) {
-			System.out.println("Scientific name matches!");
-		}	
-		
-		String numberofobservations = driver.findElement(By.cssSelector("#TaxaList > ul > li > ul > li > div > span")).toString();
-		
-		System.out.print("User has observed " + numberofobservations + "instances of taxon " + taxon);
+		//click iNaturalist logo and return to the iNaturalist main screen
+		driver.findElement(By.cssSelector("#logonav > a > img")).click();
+        driver.manage().timeouts().implicitlyWait(10, TimeUnit.SECONDS);
+        
+        String expectedURL = "https://www.inaturalist.org/home";
+        String actualURL = driver.getTitle();
+        
+        if (actualURL.contentEquals(expectedURL)) {
+        	System.out.println("Back to main screen!");
+        }else {
+        	System.out.println("Not on main screen");
+        }
 	}
+
 }
