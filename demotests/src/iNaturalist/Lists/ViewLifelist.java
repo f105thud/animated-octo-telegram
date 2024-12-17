868d61c16dc284455f514f92796fd87e4d03d2f9
Owner <Owner@DESKTOP-QCSS2MM.cable.rcn.com>
2024-12-16 22:56:02 -0500

    Deleted blank line from ViewLifelist.java

--- demotests/src/iNaturalist/Lists/ViewLifelist.java
+++ demotests/src/iNaturalist/Lists/ViewLifelist.java
@@ -4,7 +4,6 @@ import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.chrome.ChromeDriver;
 import org.openqa.selenium.chrome.ChromeOptions;
-
 import java.io.File;
 import java.io.FileInputStream;
 import java.io.IOException;
